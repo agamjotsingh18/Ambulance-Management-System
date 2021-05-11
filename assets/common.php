f99315005b17f842c774e91ecd4024b33486fbdd
Naveen <naveengali80@gmail.com>
2021-05-11 10:17:00 +0530

    Completed Login Screen

--- assets/common.php
+++ assets/common.php
@@ -1,2 +1,6 @@
 <?php
 $con = mysqli_connect("localhost","root","","ambulance_management") or die(mysqli_error($con));
+
+if (!isset($_SESSION)){
+    session_start();
+}
--- login.php
+++ login.php
@@ -1,3 +1,42 @@
 <?php
     include ("assets/common.php");
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login | Ambulance Management System</title>
+        <?php
+            require "assets/bootstrap_cdn.php";
+        ?>
+    </head>
+    <body>
+    <div class="container">
+        <div class="jumbotron" style="margin-top: 60px">
+            <div class="row">
+                <div class="col-sm-10">
+                    <h2 style="padding-left: 20px;padding-bottom: 10px">LogIn</h2>
+                    <form action="login_script.php" method="post" style="padding-bottom:300px">
+
+                        <div class="form-group col-sm-10">
+                            <input class="form-control" type="email" placeholder="Enter your registered Email" name="email" required>
+                        </div>
+
+                        <div class="form-group col-sm-10">
+                            <input class="form-control" type="password" placeholder="Enter your password" name="password" required>
+                        </div>
+
+                        <div class="col-sm-10">
+
+                            <button type="submit" name="submit" class="btn btn-primary">Submit</button><br><br>
+                        </div>
+                        <div class="col-sm-10">
+                            <p>Don't Have an Account ? Register for free. <a href="signup.php">Click here</a> to register.</p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
+
